66d57ad647803eb9e204ce3b658a50424f4122aa
Enrico Moraes Brasil Mustafa <enrico46444506@aluno.ba.senac.br>
2023-10-10 10:59:20 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Contador from '../contador/components/contado'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Contador></Contador>
     </View>
   );
 }
